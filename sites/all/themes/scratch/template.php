36648c332fc416f278914004a2ff5f9a9871c6ab
Ashlin Aronin & Alexandra Brown <student@epicodus.com>
2015-10-29 12:30:19 -0700

    Customize menu display using hook and css

--- sites/all/themes/scratch/template.php
+++ sites/all/themes/scratch/template.php
@@ -2,14 +2,14 @@
 
 /*
 ** Implements hook_links__system_main_menu().
-** Try changing main menu to an ordered list to see if it works.
+** Try changing main menu to an ordered list -- works.
 */
 function scratch_links__system_main_menu($variables) {
   $html = "<div>\n";
   $html .= " <ol>\n";
 
   foreach ($variables['links'] as $link) {
-    $html .= "<li>" . l($link['title'], $link['path'], $link) . "</li>";
+    $html .= "<li class='ordered-li'>" . l($link['title'], $link['href'], $link) . "</li>";
   }
   $html .= " </ol>\n";
   $html .= "</div>\n";

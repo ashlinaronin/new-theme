d6cb0dea622ab894ac1fdf2ccea681ddbcbfd583
Ashlin Aronin & Alexandra Brown <student@epicodus.com>
2015-10-29 11:32:48 -0700

    Get basic Zen Grid working with Sass and Compass

--- sites/all/themes/scratch/page.tpl.php
+++ sites/all/themes/scratch/page.tpl.php
@@ -1,37 +1,51 @@
-<div id="header">
-  <a href="<?php print $front_page;?>">
-    <img src="/<?php print $directory;?>/images/logo.png" alt="<?php print $site_name;?>" height="47" width="217" />
-  </a>
+<div class="container">
 
-  <?php if ($main_menu): ?>
-      <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu'))); ?>
-  <?php endif; ?>
+  <header class="header">
+    <a href="<?php print $front_page;?>">
+      <img src="/<?php print $directory;?>/images/logo.png" alt="<?php print $site_name;?>" height="47" width="217" />
+    </a>
 
-</div>
+    <?php if ($main_menu): ?>
+        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu'))); ?>
+    <?php endif; ?>
 
-<div id="wrapper">
-  <div id="content">
-    <?php print render($title_prefix); ?>
-      <?php if ($title): ?><h1><?php print $title; ?></h1><?php endif; ?>
-    <?php print render($title_suffix); ?>
+  </header>
 
-    <?php print render($messages); ?>
-    <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-    <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+  <div id="wrapper">
 
-    <?php print render($page['content']); ?>
-  </div>
 
-  <?php if ($page['sidebar_first']): ?>
-    <div id="sidebar">
-      <?php print render($page['sidebar_first']); ?>
-    </div>
-  <?php endif; ?>
+    <article class="content">
+      <?php print render($title_prefix); ?>
+        <?php if ($title): ?><h1><?php print $title; ?></h1><?php endif; ?>
+      <?php print render($title_suffix); ?>
 
-</div>
+      <?php print render($messages); ?>
+      <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
 
-  <div id="footer">
+      <?php print render($page['content']); ?>
+    </article>
+
+    <?php if ($page['sidebar_left']): ?>
+      <aside class="sidebar_left">
+        <?php print render($page['sidebar_left']); ?>
+      </aside>
+    <?php endif; ?>
+
+    <?php if ($page['sidebar_right']): ?>
+      <aside class="sidebar_right">
+        <?php print render($page['sidebar_right']); ?>
+      </aside>
+    <?php endif; ?>
+
+
+  </div> <!-- end wrapper -->
+
+
+  <footer class="footer">
     <?php if ($page['footer']): ?>
       <?php print render($page['footer']); ?>
     <?php endif; ?>
-  </div>
+  </footer>
+
+</div> <!-- end container -->
